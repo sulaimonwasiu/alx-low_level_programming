9ae9ed163b279ddc51b33ec7b96fe89056c5b3c6
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2023-07-13 09:58:59 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function prints a string to the console using the printf function.
+ *
+ * Return: 0 on success, non-zero on failure
+ */
+int main(void)
+{
+/* Use the printf function to output a string to the console */
+printf("with proper grammar, but the outcome is a piece of art,\n");
+
+/* Return 0 to indicate successful completion */
+return (0);
+}
