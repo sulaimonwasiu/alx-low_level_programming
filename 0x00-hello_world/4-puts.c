c55e8f85d2be554b23cd3dd46406be460f5dfaa3
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2023-07-13 09:47:17 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * This function prints a string to the console using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
+
+/* Use the puts function to output a string to the console */
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
+
 }
