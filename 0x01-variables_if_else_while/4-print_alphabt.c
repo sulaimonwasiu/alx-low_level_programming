b6043e0e60a4acd66ff485f6799eecd392b489c4
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2023-07-14 20:20:15 -0700

    Print alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print lower case [a-z] without q and e
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
