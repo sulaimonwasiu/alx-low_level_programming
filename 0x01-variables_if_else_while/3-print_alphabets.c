ef7b60d2ae92f87841c1ff7e897707a5bf154c67
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2023-07-14 20:11:32 -0700

    Print upper and lower case alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints upper and lower case alpha
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	letter = 'A';
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
