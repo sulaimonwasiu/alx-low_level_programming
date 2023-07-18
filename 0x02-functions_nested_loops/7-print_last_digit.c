57750ede012ecc6f137a761bef7256f3135c6038
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2023-07-19 01:51:16 +0300

    Print last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of number
+ * @n: digit inputed
+ * Return: the last digit of inputed number
+ */
+
+int print_last_digit(int n)
+{
+	n %= 10;
+
+	if (n < 0)
+	{
+		n *= (-1);
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
