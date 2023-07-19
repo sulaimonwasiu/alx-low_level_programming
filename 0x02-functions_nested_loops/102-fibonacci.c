6a7ca3ed3e59879f3295701e9bfb27b526840558
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2023-07-20 01:12:51 +0300

    First 50 fibonacci series

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int first = 1, int second = 2, count = 2, store;
+
+	printf("%d, ", first);
+	printf("%d, ", second);
+
+	while (count <= 50)
+	{
+		store = second;
+		second = second + first;
+		first = store;
+		printf("%d, ", second);
+		count++;
+	}
+	return (0);
+}
