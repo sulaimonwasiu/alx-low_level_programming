f4d484cfc47ab68662f7a4a71783750e271ff671
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2023-07-20 02:13:02 +0300

    Sum of even fibonacci terms

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int first = 1, second = 2, store, sum;
+
+	sum = second;
+	while (second <= 4000000)
+	{
+		store = second;
+		second = second + first;
+		first = store;
+		if (second % 2 == 0)
+		{
+			sum += second;
+		}
+	}
+	printf("%d", sum);
+	printf("\n");
+	return (0);
+}
