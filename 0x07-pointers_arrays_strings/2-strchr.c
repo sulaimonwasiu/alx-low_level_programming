0cf81295af6f002d485bfdaec6b9f8d079e3b462
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2023-07-31 15:58:16 +0300

    .

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,9 +9,14 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0' && *s != c)
+	while (*s != '\0')
 	{
+		if (*s == c)
+			return (s);
+		else if (*(s + 1) == c)
+			return (s + 1);
 		s++;
 	}
-	return ((*s == c) ? s : NULL);
+
+	return (s + 1);
 }
