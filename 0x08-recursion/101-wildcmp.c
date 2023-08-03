f5bdf4f1203e881ed76a7d3a5de1209d4f66bc4c
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2023-08-02 22:17:59 -0700

    Last attempt

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,30 +16,11 @@ if (*s1 == '*' && *(s1 + 1) == '\0')
 	return (1);
 if (*s2 == '*' && *(s2 + 1) == '\0')
 	return (1);
-
 if (*s1 == *s2)
 	return (wildcmp(s1 + 1, s2 + 1));
-
 if (*s1 == '*')
-{
-while (*s2 != '\0')
-{
-if (wildcmp(s1 + 1, s2) == 1)
-	return (1);
-s2++;
-}
-}
-
+	return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 if (*s2 == '*')
-{
-while (*s1 != '\0')
-{
-if (wildcmp(s1, s2 + 1) == 1)
-	return (1);
-s1++;
-}
-}
-
+	return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 return (0);
-
 }
