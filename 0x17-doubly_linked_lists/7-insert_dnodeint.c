3eae255464e18decd855f3e8b0e7206b9d3cae0c
sulaimonwasiu <sulaimonwasiu13.gmai.com>
2023-12-08 01:42:09 -0800

    Edit task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,7 +7,7 @@
  * @n: value to store in new node
  * Return: Address of new node, or NULL if failed
  */
-dlistint_t *insert_dnodeint_at_idx(dlistint_t **h, unsigned int idx, int n)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i;
 
