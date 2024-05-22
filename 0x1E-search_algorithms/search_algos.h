6d9f6a56eecdd53888dddc8966d825094f1874f2
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2024-05-22 12:16:55 -0700

    Binary search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,7 +2,6 @@
 #define SEARCH_ALGO_H
 #include <stdio.h>
 int linear_search(int *array, size_t size, int value);
-int rec_binary_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 #endif
